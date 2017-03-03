8af15d72665ff99ddeaf054872fbd1cce91e145a
joy1011 <joys_bao@qq.com>
2017-03-03 17:11:36 +0800

    baidumap test html

--- FlyControl/MainWindow.xaml.cs
+++ FlyControl/MainWindow.xaml.cs
@@ -12,17 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Permissions;
 
 namespace FlyControl
 {
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            string str_url = Environment.CurrentDirectory + "\\test.html";
+            Uri url = new Uri(str_url);
+            webBrowser.Navigate(url);
+            //webBrowser.ObjectForScripting = this;
         }
     }
 }
